c11cb0044278f684c20646c8deea81547cde6104
Brandon LeBoeuf <BrandonL@henry-v.com>
2021-12-30 12:15:45 -0800

    cleans up link tag layouts (un-prettier)

--- pages/3dvista/index.jsx
+++ pages/3dvista/index.jsx
@@ -6,51 +6,17 @@ export default function index() {
       <Head>
         <title>Untitled 2</title>
         <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
-        <meta
-          name="viewport"
-          id="metaViewport"
-          content="user-scalable=no, initial-scale=1, width=device-width, viewport-fit=cover"
-          data-tdv-general-scale="0.5"
-        />
+        <meta name="viewport" id="metaViewport" content="user-scalable=no, initial-scale=1, width=device-width, viewport-fit=cover" data-tdv-general-scale="0.5"/>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <link
-          rel="preload"
-          href="locale/en.txt"
-          as="fetch"
-          crossOrigin="anonymous"
-        />
+        <link rel="preload" href="locale/en.txt" as="fetch" crossOrigin="anonymous" />
         <link rel="preload" href="./script.js" as="script" />
-        <link
-          rel="preload"
-          href="media/panorama_06775DB7_0C2E_D9EB_41A3_14479EFA84DD_0/r/1/0_0.jpg"
-          as="image"
-        />
-        <link
-          rel="preload"
-          href="media/panorama_06775DB7_0C2E_D9EB_41A3_14479EFA84DD_0/l/1/0_0.jpg"
-          as="image"
-        />
-        <link
-          rel="preload"
-          href="media/panorama_06775DB7_0C2E_D9EB_41A3_14479EFA84DD_0/u/1/0_0.jpg"
-          as="image"
-        />
-        <link
-          rel="preload"
-          href="media/panorama_06775DB7_0C2E_D9EB_41A3_14479EFA84DD_0/d/1/0_0.jpg"
-          as="image"
-        />
-        <link
-          rel="preload"
-          href="media/panorama_06775DB7_0C2E_D9EB_41A3_14479EFA84DD_0/f/1/0_0.jpg"
-          as="image"
-        />
-        <link
-          rel="preload"
-          href="media/panorama_06775DB7_0C2E_D9EB_41A3_14479EFA84DD_0/b/1/0_0.jpg"
-          as="image"
-        />
+        <link rel="preload" href="media/panorama_06775DB7_0C2E_D9EB_41A3_14479EFA84DD_0/r/1/0_0.jpg" as="image" />
+        <link rel="preload" href="media/panorama_06775DB7_0C2E_D9EB_41A3_14479EFA84DD_0/l/1/0_0.jpg" as="image" />
+        <link rel="preload" href="media/panorama_06775DB7_0C2E_D9EB_41A3_14479EFA84DD_0/u/1/0_0.jpg" as="image" />
+        <link rel="preload" href="media/panorama_06775DB7_0C2E_D9EB_41A3_14479EFA84DD_0/d/1/0_0.jpg" as="image" />
+        <link rel="preload" href="media/panorama_06775DB7_0C2E_D9EB_41A3_14479EFA84DD_0/f/1/0_0.jpg" as="image" />
+        <link rel="preload" href="media/panorama_06775DB7_0C2E_D9EB_41A3_14479EFA84DD_0/b/1/0_0.jpg" as="image" />
         <meta name="description" content="Virtual Tour" />
         <meta name="theme-color" content="#FFFFFF" />
         <script src="lib/tdvplayer.js"></script>
